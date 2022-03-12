415adf65c7ab3c7d79ee5394315d9c69cbaa5661
sakamoto-waka <wakanmhl@gmail.com>
2022-03-12 00:54:27 +0000

    clear all commit

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  get 'chats/show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root :to =>"homes#top"
+  get "home/about"=>"homes#about"
+  devise_for :users
+
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:index,:show,:edit,:update] do
+        resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  get "search" => "searches#search"
+  get 'chat/:id' => 'chats#show', as: 'chat'
+  resources :chats, only: [:create]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
