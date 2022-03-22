69992a4447e01ef768dc100c7f264d4814bf3371
sakamoto-waka <wakanmhl@gmail.com>
2022-03-22 01:27:05 +0000

    [update] sessions_controller.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,8 +26,13 @@ class User < ApplicationRecord
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
-
-
+  
+  def self.guest
+    find_or_create_by!(name: 'guestuser', email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = 'guestuser'
+    end  
+  end  
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
@@ -58,4 +63,5 @@ class User < ApplicationRecord
       User.where('name LIKE ?', "%#{word}%")
     end
   end
+  
 end
